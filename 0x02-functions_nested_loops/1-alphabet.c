7463dd62931e9905ab9d7b313cc1c1c516c2ea86
agbajeolayiwola <agbajeolaiwola@gmail.com>
2022-04-14 00:07:17 +0100

    'loops'

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+   	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		++ch;
+	}
+
+	_putchar('\n');
+}
