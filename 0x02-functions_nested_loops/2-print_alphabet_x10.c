99ceccb94582a3fb91da62b232b74995df80e6b0
agbajeolayiwola <agbajeolaiwola@gmail.com>
2022-04-14 01:38:37 +0100

    'loops'

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet_x10 - Make alphabet x10 times
  * Description: A function that prints 10 times the alphabet, in lowercase
  * Return: void
  */
