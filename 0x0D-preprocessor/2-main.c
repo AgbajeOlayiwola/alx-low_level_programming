09a1dbc92310d8ddcee38f09a59d68f21e33c14c
agbajeolayiwola <agbajeolaiwola@gmail.com>
2022-05-13 01:23:25 +0100

    'macro'

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * Print the name of the file
+ * main - prints the name of the file.
  *
  * Return: Always 0.
  */
-
-int main(void){
-    putchar("%s\n",__FILE__);
-    return (0);
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
 }
